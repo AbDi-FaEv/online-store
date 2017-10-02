de95dfdf97ae3696060475ddd4f77a90cdb8ed77
Dmitry Abramenko <abramenko@a2design.biz>
2017-10-02 23:12:21 +0600

    init project

---
+++ resources/views/admin/devices/index.blade.php
@@ -0,0 +1,36 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="panel-title">
+                            Devices
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-responsive">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th class="text-right">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($devices as $device)
+                                <tr>
+                                    <td>{{ $device->name }}</td>
+                                    <td class="text-right">
+                                        <a class="btn btn-danger" href="#">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
